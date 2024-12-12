a2d2ed2de6ae443acb48d814dba96e7699345dbd
ref1821 <ref1821@gmail.com>
2024-12-11 22:44:32 -0600

    v1.02

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -10,13 +10,13 @@ function App() {
                 <Route path="/TMR24" element={<Page loc='TMR24.stl'/>} />
                 <Route path="/Chassis" element={<Page loc='Chassis.stl'/>} />
                 <Route path="/Suspension" element={<Page loc='Suspe.stl'/>} />
-                <Route path="/Brake-system" element={<div>Brake System</div>} />
-                <Route path="/Powertrain" element={<div>Powertrain</div>} />
-                <Route path="/Cooling" element={<div>Cooling</div>} />
-                <Route path="/Electronics" element={<div>Electronics</div>} />
-                <Route path="/Front-wing" element={<div>Alerón Delantero</div>} />
-                <Route path="/Rear-wing" element={<div>Alerón Trasero</div>} />
-                <Route path="/Diffusor" element={<div>Difusor</div>} />
+                <Route path="/Brake-system" element={<Page loc='Brake-system.stl'/>} />
+                <Route path="/Powertrain" element={<Page loc='Powertrain.stl'/>} />
+                <Route path="/Cooling" element={<Page loc='Cooling.stl'/>} />
+                <Route path="/Electronics" element={<Page loc='Electronics.stl'/>} />
+                <Route path="/Front-wing" element={<Page loc='Front-wing.stl'/>} />
+                <Route path="/Rear-wing" element={<Page loc='Rear-wing.stl'/>} />
+                <Route path="/Diffusor" element={<Page loc='Diffusor.stl'/>} />
             </Routes>
         </Router>
     );
