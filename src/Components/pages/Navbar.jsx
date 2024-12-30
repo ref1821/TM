c3c8a6d7cd2094088f48570f8cb7e65ac6f18916
ref1821 <ref1821@gmail.com>
2024-12-30 00:21:13 -0600

    v1.13

--- src/Components/pages/Navbar.jsx
+++ src/Components/pages/Navbar.jsx
@@ -1,113 +1,224 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoCarSportOutline } from "react-icons/io5";
 import { LuBox } from "react-icons/lu";
 import { GiSpring, GiCarWheel } from "react-icons/gi";
 import { PiEngine, PiThermometerCold } from "react-icons/pi";
 import { SlEnergy } from "react-icons/sl";
 import AirIcon from '@mui/icons-material/Air';
-
+import { AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const handleButtonClick = (component) => {
         const urlPath = component.replace(/\s+/g, '-').toLowerCase();
         window.location.href = `/${urlPath}`;
     };
 
     return (
-        <nav className="bg-gray-100 font-montserrat text-md">
+        <div>
+            {/* Menu button for mobile */}
             <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("TMR24")}
+                className={`sm:hidden fixed bg-gray-200 p-3 rounded-full shadow-lg z-50 transform ${isMenuOpen ? "top-2 left-2" : "top-35 left-4"}`}
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-                <div className="flex justify-center">
-                    <IoCarSportOutline />
-                </div>
-                <p>TMR24</p>
+                <AiOutlineMenu />
             </button>
 
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Chassis")}
+            {/* Sidebar menu */}
+            <div
+                className={`fixed top-0 left-0 h-full bg-gray-100 shadow-lg transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 z-40 sm:hidden`}
             >
-                <div className="flex justify-center">
-                    <LuBox />
-                </div>
-                <p>Chassis</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Suspension")}
-            >
-                <div className="flex justify-center">
-                    <GiSpring />
-                </div>
-                <p>Suspension</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Brake-system")}
-            >
-                <div className="flex justify-center">
-                    <GiCarWheel />
-                </div>
-                <p>Brake system</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Powertrain")}
-            >
-                <div className="flex justify-center">
-                    <PiEngine />
-                </div>
-                <p>Powertrain</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Cooling")}
-            >
-                <div className="flex justify-center">
-                    <PiThermometerCold />
-                </div>
-                <p>Cooling</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Electronics")}
-            >
-                <div className="flex justify-center">
-                    <SlEnergy /> 
-                </div>
-                <p>Electronics</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Front-wing")}
-            >
-                <div className="flex justify-center">
-                    <AirIcon />
-                </div>
-                <p>Front wing</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Rear-wing")}
-            >
-                <div className="flex justify-center">
-                    <AirIcon />
-                </div>
-                <p>Rear wing</p>
-            </button>
-            <button
-                className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
-                onClick={() => handleButtonClick("Diffusor")}
-            >
-                <div className="flex justify-center">
-                    <AirIcon />
-                </div>
-                <p>Diffuser</p>
-            </button>
-</nav>
+                <nav className="font-montserrat text-md p-4">
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("TMR24")}
+                    >
+                        <div className="flex justify-center">
+                            <IoCarSportOutline />
+                            <p className='ml-2'>TMR24</p>
+                        </div>
+                    </button>
 
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Chassis")}
+                    >
+                        <div className="flex justify-center">
+                            <LuBox />
+                            <p className='ml-2'>Chassis</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Suspension")}
+                    >
+                        <div className="flex justify-center">
+                            <GiSpring />
+                            <p className='ml-2'>Suspension</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Brake-system")}
+                    >
+                        <div className="flex justify-center">
+                            <GiCarWheel />
+                            <p className='ml-2'>Brake system</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Powertrain")}
+                    >
+                        <div className="flex justify-center">
+                            <PiEngine />
+                            <p className='ml-2'>Powertrain</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Cooling")}
+                    >
+                        <div className="flex justify-center">
+                            <PiThermometerCold />
+                            <p className='ml-2'>Cooling</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Electronics")}
+                    >
+                        <div className="flex justify-center">
+                            <SlEnergy /> 
+                            <p className='ml-2'>Electronics</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Front-wing")}
+                    >
+                        <div className="flex justify-center">
+                            <AirIcon />
+                            <p className='ml-2'>Front wing</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Rear-wing")}
+                    >
+                        <div className="flex justify-center">
+                            <AirIcon />
+                            <p className='ml-2'>Rear wing</p>
+                        </div>
+                    </button>
+                    <button
+                        className="block m-3 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                        onClick={() => handleButtonClick("Diffusor")}
+                    >
+                        <div className="flex justify-center">
+                            <AirIcon />
+                            <p className='ml-2'>Diffuser</p>
+                        </div>
+                    </button>
+                </nav>
+            </div>
+
+            {/* Full navbar for larger screens */}
+            <nav className="hidden sm:block bg-gray-100 font-montserrat text-md">
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("TMR24")}
+                >
+                    <div className="flex justify-center">
+                        <IoCarSportOutline />
+                    </div>
+                    <p>TMR24</p>
+                </button>
+
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Chassis")}
+                >
+                    <div className="flex justify-center">
+                        <LuBox />
+                    </div>
+                    <p>Chassis</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Suspension")}
+                >
+                    <div className="flex justify-center">
+                        <GiSpring />
+                    </div>
+                    <p>Suspension</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Brake-system")}
+                >
+                    <div className="flex justify-center">
+                        <GiCarWheel />
+                    </div>
+                    <p>Brake system</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Powertrain")}
+                >
+                    <div className="flex justify-center">
+                        <PiEngine />
+                    </div>
+                    <p>Powertrain</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Cooling")}
+                >
+                    <div className="flex justify-center">
+                        <PiThermometerCold />
+                    </div>
+                    <p>Cooling</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Electronics")}
+                >
+                    <div className="flex justify-center">
+                        <SlEnergy /> 
+                    </div>
+                    <p>Electronics</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Front-wing")}
+                >
+                    <div className="flex justify-center">
+                        <AirIcon />
+                    </div>
+                    <p>Front wing</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Rear-wing")}
+                >
+                    <div className="flex justify-center">
+                        <AirIcon />
+                    </div>
+                    <p>Rear wing</p>
+                </button>
+                <button
+                    className="m-5 p-3 bg-gray-100 border-b-2 border-gray-300 rounded-md hover:border-gray-300 hover:bg-gray-300"
+                    onClick={() => handleButtonClick("Diffusor")}
+                >
+                    <div className="flex justify-center">
+                        <AirIcon />
+                    </div>
+                    <p>Diffuser</p>
+                </button>
+            </nav>
+        </div>
     );
 }
 
--- src/Components/pages/Page.jsx
+++ src/Components/pages/Page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import {Showcase} from '../Showcase'
+import { Link } from '@mui/material'
 
 export const Page = (props) => {
   return (
@@ -15,17 +16,23 @@ export const Page = (props) => {
         <Navbar/>
       </div>
       <Showcase location={props.loc} scale={props.scale} x={props.x} y={props.y} z={props.z}/>
-      <div className="fixed bottom-4 right-4 w-64 p-4 bg-white shadow-lg rounded-lg z-50">
+      <a href="https://www.instagram.com/tmracingteam?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+  <div className="fixed bottom-0 w-full sm:bottom-4 sm:right-4 sm:w-64 p-4 bg-white shadow-lg rounded-none sm:rounded-lg z-50 text-center">
+    {/* Imagen, visible solo en pantallas más grandes */}
+    <img
+      src="/qrinsta.png"
+      alt="QR Instagram"
+      className="w-full h-auto rounded-md hidden sm:block"
+    />
+    {/* Texto, siempre visible */}
+    <p className="text-center sm:hidden">Síguenos en Instagram</p>
+    <p className="text-center hidden sm:block">Síguenos en Instagram</p>
+  </div>
+</a>
+
+      <div className="fixed bottom-4 left-4 w-64 p-4 bg-white shadow-lg rounded-lg z-50 hidden sm:block">
         <img
-          src="/qrinsta.png" // Ruta de la imagen en la carpeta `public`
-          alt="Descripción"
-          className="w-full h-auto rounded-md"
-        />
-        <p className="text-center">Siguenos en Instagram</p>
-      </div>
-      <div className="fixed bottom-4 left-4 w-64 p-4 bg-white shadow-lg rounded-lg z-50">
-        <img
-          src="/qr.png" // Ruta de la imagen en la carpeta `public`
+          src="/qr.png"
           alt="Descripción"
           className="w-full h-auto rounded-md"
         />
